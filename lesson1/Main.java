3038915e6dbaecec13e41f7ae68743a860f939db
Vadim Mikhalin <vsmikhalin@gmail.com>
2020-01-12 01:47:00 +0300

    Добавлены файлы

---
+++ lesson1/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+    //TODO: просуммировать числа от 1 до 5
+        /*
+        1+2+3+4+5=15
+        1+2=3
+        3+3=6
+        6+4=10
+        10+5=15
+        */
+        int sum = 1+2;
+        System.out.println(sum);
+    }
+}
