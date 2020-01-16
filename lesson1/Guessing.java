78682240bd530fe32fa9f8ae5c556073c07d03f0
Vadim Mikhalin <vsmikhalin@gmail.com>
2020-01-17 01:07:22 +0300

    Удаление файлов Guessing.java, Main.java, UnicodeSymbols.java

--- lesson1/Guessing.java
+++
@@ -1,7 +0,0 @@
-public class UnicodSymbols {
-    public static void main(String[] args) {
-    	for (int i = 9398; i <= 9399; i++){
-    		System.out.println (string ("\\" + i));
-    	}
-    }
-}
--- lesson1/UnicodeSymbols.java
+++
@@ -1,7 +0,0 @@
-public class UnicodeSymbols {
-    public static void main(String[] args) {
-    	for (int i = 9398; i <= 10178; i++){
-    		System.out.println ((char)i);
-    	}
-    }
-}
