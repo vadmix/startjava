503e4b60fcc3ed7bd5e6d7761be801fdcf973e5a
Vadim Mikhalin <vsmikhalin@gmail.com>
2020-01-20 19:57:51 +0300

    Обновлен ConditionalStatement.java

--- lesson1/ConditionalStatement.java
+++ lesson1/ConditionalStatement.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; // подключаем сканер
 
 public class ConditionalStatement {
-	public static void main (String[] args){
+	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
 		System.out.println("Укажите Ваш возраст:");
