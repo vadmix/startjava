b4990c3c2e8555107a1e1f4928c673102a657b51
Vadim Mikhalin <vsmikhalin@gmail.com>
2020-01-27 22:07:02 +0300

    Добавлены Jaeger.java и JaegerTest.java

---
+++ lesson2/Jaeger.java
@@ -0,0 +1,70 @@
+public class Jaeger {
+
+	private String modelName;
+	private String mark;
+	private float weight;
+	private float height;
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public void takeGun() {
+		System.out.println(modelName + " - Оружие в руке!");
+	}
+
+	public void putGunAway() {
+		System.out.println(modelName + " - Оружие в кобуре!");
+	}
+
+	public void takeAim() {
+		System.out.println(modelName + " - Враг на мушке!");
+	}
+
+	public void fireAway() {
+		System.out.println(modelName + " - Пли!");
+	}
+
+	public void shoot() {
+		takeGun();
+		takeAim();
+		fireAway();
+	}
+
+	public void move() {
+		System.out.println(modelName + " - Идем!");
+	}
+
+	public String signal() {
+		System.out.println(modelName + " - Загорелась красная лампочка!!!");
+		return "Уиу-уиу-уиу";
+	}
+}
---
+++ lesson2/JaegerTest.java
@@ -0,0 +1,25 @@
+public class JaegerTest {
+	public static void main(String[] args) {
+		Jaeger jaeger1 = new Jaeger();
+		jaeger1.setModelName("Bracer Phoenix");
+		jaeger1.setMark("Mark-5");
+		jaeger1.setWeight(2.128f);
+		jaeger1.setHeight(70.91f);
+
+		Jaeger jaeger2 = new Jaeger();
+		jaeger2.setModelName("November Ajax");
+		jaeger2.setMark("Mark-6");
+		jaeger2.setWeight(2.478f);
+		jaeger2.setHeight(78.94f);
+
+		jaeger1.move();
+		jaeger1.takeGun();
+		System.out.println(jaeger2.signal());
+		jaeger2.shoot();
+		jaeger1.setWeight(1.453f);
+		jaeger1.setHeight(50.32f);
+		jaeger2.putGunAway();
+		System.out.println(jaeger1.getModelName() + " - рост: " + jaeger1.getHeight());
+		System.out.println(jaeger1.getModelName() + " - вес: " + jaeger1.getWeight());
+	}
+}
